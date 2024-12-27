cbd8e47bd1eb4b6bb0b5146a1a273e98cbfb7593
Bleron Osmani <bleron.osmani@xponentl.ai>
2024-12-27 15:56:24 +0100

    dag changes

--- dags/healthcare.py
+++ dags/healthcare.py
@@ -6,7 +6,7 @@ from datetime import datetime, timedelta
 default_args = {
     'owner': 'airflow',
     'depends_on_past': False,
-    'start_date': datetime(2024, 12, 26),
+    'start_date': datetime(2024, 12, 26),  # Updated start date for testing
     'email_on_failure': False,
     'email_on_retry': False,
     'retries': 1,
@@ -15,23 +15,23 @@ default_args = {
 
 # Define the DAG
 dag = DAG(
-    'dbt_healthcare_dag',
+    'healthcare_dag',
     default_args=default_args,
-    description='A DAG to run dbt models for healthcare project',
-    schedule_interval=timedelta(days=1),
+    description='A Healthcare DAG',
+    schedule=timedelta(days=1),  # Runs daily
 )
 
 # Task to run the silver model
 dbt_run_silver = BashOperator(
     task_id='dbt_run_silver',
-    bash_command='dbt run --models silver_healthcare_data --profiles-dir /usr/local/airflow/dags/dbt_project --project-dir /usr/local/airflow/dags/dbt_project',
+    bash_command='dbt run --models silver_healthcare_data --profiles-dir /usr/local/airflow/dags/healthcare_project --project-dir /usr/local/airflow/dags/healthcare_project',
     dag=dag,
 )
 
 # Task to run the gold model
 dbt_run_gold = BashOperator(
     task_id='dbt_run_gold',
-    bash_command='dbt run --models gold_healthcare_diagnosis_summary --profiles-dir /usr/local/airflow/dags/dbt_project --project-dir /usr/local/airflow/dags/dbt_project',
+    bash_command='dbt run --models gold_healthcare_diagnosis_summary --profiles-dir /usr/local/airflow/dags/healthcare_project --project-dir /usr/local/airflow/dags/healthcare_project',
     dag=dag,
 )
 
